e01ca7ba6562f967f14ea31b9bd734f9acaa1149
btbj <hbjbtbj@gmail.com>
2024-09-05 16:19:11 +0800

    init set up

---
+++ Sources/MyHelloView/MyHelloView.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by lll on 2024/9/5.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct MyHelloView: View {
+    public var body: some View {
+        Text("ttt Hello View!")
+    }
+    public init(){
+        
+    }
+}
+
+@available(iOS 13.0, *)
+#Preview {
+    MyHelloView()
+}
